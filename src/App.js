dde2e463cd5380dd2893daeb18194e50214f4895
ashwin-rema <ashwinrema111@gmail.com>
2022-01-10 16:59:58 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import FormComponent from './FormComponent';
+import Title from './Title';
 function App() {
   return (
     <div className="App">
       <h1>PSG College of Technolgoy</h1>
+      <Title />
       <FormComponent />
     </div>
   );
--- src/Title.js
+++ src/Title.js
@@ -1,1 +1,27 @@
+
 import React from 'react';
+import { UncontrolledCarousel } from 'reactstrap';
+
+const items = [
+  {
+    src: 'logo.svg',
+    altText: 'Slide 1',
+    caption: 'Slide 1',
+    header: 'Slide 1 Header'
+  },
+  {
+    src: 'logo.svg',
+    altText: 'Slide 2',
+    caption: 'Slide 2',
+    header: 'Slide 2 Header'
+  },
+  {
+    src: 'logo.svg',
+    altText: 'Slide 3',
+    caption: 'Slide 3',
+    header: 'Slide 3 Header'
+  }
+];
+
+const Title = () => <UncontrolledCarousel items={items} />;
+export default Title;
